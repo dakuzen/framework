b4dcd9bc22e9e0805a54a228d96d00d42fa4f433
Raymond <ray@firestitch.com>
2016-01-04 14:57:17 -0500

    allowed downloading of backups

--- templates/manage/backups.php
+++ templates/manage/backups.php
@@ -3,24 +3,25 @@
 	$table_data = array();	
 	foreach($ff_backups as $ff_backup) {
 		
-		$table_data[] = array(	FORMAT_UTIL::get_long_date_time($ff_backup->get_start_time()),
-								$ff_backup->get_state_name(),
-								round($ff_backup->get_duration(),2),
-								$ff_backup->get_filename(),
-								FORMAT_UTIL::get_formatted_filesize($ff_backup->get_filesize()),
-								$ff_backup->get_message());
+		$backup = FORMAT_UTIL::get_long_date_time($ff_backup->get_start_time()).HTML_UTIL::span(round($ff_backup->get_duration(),2)." seconds",array("class"=>"fssr ml5"));
+
+		if($message=$ff_backup->get_message())
+			$backup .= HTML_UTIL::div($message);
+
+		$table_data[] = array(	$backup,
+								$ff_backup->get_state_name(),								
+								HTML_UTIL::a($this->get_task_url()."bid:".$ff_backup->get_ff_backup_id()."/action:download",$ff_backup->get_filename(),array("target"=>"_blank")),
+								FORMAT_UTIL::get_formatted_filesize($ff_backup->get_filesize()));
 	}
 
 		
-	$html_table = new HTML_TABLE_UTIL();
-	$html_table->set_data($table_data);
-	$html_table->set_headings(array("Backup Date","State","Duration (seconds)","File Name","File Size","Message"));
-	$html_table->set_column_attribute(1,"align","center");
-	$html_table->set_column_attribute(2,"align","center");
-	$html_table->set_column_attribute(0,"nowrap","nowrap");
-	$html_table->set_column_attribute(4,"nowrap","nowrap");	
-	$html_table->set_width("100%");
-	$html_table->render();
+	HTML_TABLE_UTIL::create()
+		->set_data($table_data)
+		->set_headings(array("Backup","Status","File Name","File Size"))
+		->set_column_attribute(1,"align","center")
+		->set_column_attribute(2,"align","center")
+		->set_width("100%")
+		->render();
 	
 ?>
 <script>
