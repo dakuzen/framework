4768b6bfc12cc70a50fe44afadbf0da9f1df6a79
Raymond <ray@firestitch.com>
2017-01-02 12:11:32 -0500

    renamed site content and site widgets to FF

--- templates/manage/content_widget.php
+++ templates/manage/content_widget.php
@@ -1,42 +1,42 @@
 
 <form action="javascript:;" id="content-widget-form">
 	<?
-		
-		if($site_content_widget->is_tag_readonly()) {
-			$data[] = array("Tag:",$site_content_widget->get_tag().HTML_UTIL::hidden("form[tag]",$site_content_widget->get_tag()));
-			$data[] = array("Name:",HTML_UTIL::input("form[name]",$site_content_widget->get_name(),array("class"=>"w50p")));
+
+		if($content_widget->is_tag_readonly()) {
+			$data[] = array("Tag:",$content_widget->get_tag().HTML_UTIL::hidden("form[tag]",$content_widget->get_tag()));
+			$data[] = array("Name:",HTML_UTIL::input("form[name]",$content_widget->get_name(),array("class"=>"w50p")));
 		} else {
-			$data[] = array("Name:",HTML_UTIL::input("form[name]",$site_content_widget->get_name(),array("class"=>"w50p")));
-			$data[] = array("Tag:",HTML_UTIL::input("form[tag]",$site_content_widget->get_tag(),array("class"=>"w50p")));
+			$data[] = array("Name:",HTML_UTIL::input("form[name]",$content_widget->get_name(),array("class"=>"w50p")));
+			$data[] = array("Tag:",HTML_UTIL::input("form[tag]",$content_widget->get_tag(),array("class"=>"w50p")));
 		}
-			
+
 		HTML_TABLE_UTIL::create()
 			->set_data($data)
 			->disable_css()
 			->set_column_attribute(0,"class","wsnw w1")
 			->set_width("100%")
-			->render();			
+			->render();
 	?>
 
-	<div class="coder"><?=XSS_UTIL::encode($site_content_widget->get_content())?></div>
+	<div class="coder"><?=XSS_UTIL::encode($content_widget->get_content())?></div>
 
 	<?=HTML_UTIL::link("javascript:;","Save",array("class"=>"btn btn-primary save mt10"))?>
 
-	<?=HTML_UTIL::hidden("scwid",$site_content_widget->get_site_content_widget_id())?>
+	<?=HTML_UTIL::hidden("cwid",$content_widget->get_content_widget_id())?>
 	<?=HTML_UTIL::hidden("validate",0)?>
 </form>
 
 <script>
-	
-	$(function() {	
+
+	$(function() {
 
 		$(".coder").coder({ mode: "html", name: "content" });
-		
+
 		$(".save")
 		.go("<?=$view_url?>action:save/",
-			{ 	data: "#content-widget-form", 
+			{ 	data: "#content-widget-form",
 				message: "Successfully saved the widget" });
 	});
-				
+
 </script>
 
--- templates/manage/content_widgets.php
+++ templates/manage/content_widgets.php
@@ -1,28 +1,27 @@
 <?
 	$table_data = array();
 
-	foreach($site_content_widgets as $site_content_widget) {
+	foreach($content_widgets as $content_widget) {
 
 		$actions = array();
-		$preview_link = HTML_UTIL::get_link("javascript:show_box('".$view."preview:1/scwid:".$site_content_widget->get_site_content_widget_id()."/','80%','60%')",$preview_url_html);
-		
-		$actions[] = $preview_link;
-		$actions[] = HTML_UTIL::get_link(sprintf($edit_url,$site_content_widget->get_site_content_widget_id()),$edit_url_html);
-		$actions[] = HTML_UTIL::get_link(sprintf($delete_url,$site_content_widget->get_site_content_widget_id()),$delete_url_html,array("onclick"=>"return confirm('Are you sure you would like to delete this content widget?');"));
-		
-		$content = STRING_UTIL::shorten(trim(strip_tags($site_content_widget->get_content(false))),100);
+		$preview_link = HTML_UTIL::get_link("javascript:show_box('".$view."preview:1/cwid:".$content_widget->get_content_widget_id()."/','80%','60%')",$preview_url_html);
 
-		$name = HTML_UTIL::link(sprintf($edit_url,$site_content_widget->get_site_content_widget_id()),$site_content_widget->get_name()).
-				HTML_UTIL::span("(".$site_content_widget->get_tag().")",array("class"=>"fssr ml5")).
-				HTML_UTIL::div($site_content_widget->get_path(),array("class"=>"fssr"));
-		
-		$table_data[] = array($name,$content,array("data"=>implode(" ",$actions),"class"=>"wsnw tar"));
+		$actions[] = $preview_link;
+		$actions[] = HTML_UTIL::get_link(sprintf($edit_url,$content_widget->get_content_widget_id()),$edit_url_html);
+		$actions[] = HTML_UTIL::get_link(sprintf($delete_url,$content_widget->get_content_widget_id()),$delete_url_html,array("onclick"=>"return confirm('Are you sure you would like to delete this content widget?');"));
+
+		$content = STRING_UTIL::shorten(trim(strip_tags($content_widget->get_content(false))),100);
+
+		$name = HTML_UTIL::link(sprintf($edit_url,$content_widget->get_content_widget_id()),$content_widget->get_name()).
+				HTML_UTIL::div($content_widget->get_path(),array("class"=>"fssr"));
+
+		$table_data[] = array($name,$content,$content_widget->get_tag(),array("data"=>implode(" ",$actions),"class"=>"wsnw tar"));
 	}
 
 	HTML_TABLE_UTIL::create()
 		->set_data($table_data)
-		->set_headings(array("Name","Content",""))
+		->set_headings(array("Name","Content","Tag",""))
 		->set_width("100%")
 		->render();
-	
+
 	$this->show_view("paging");
